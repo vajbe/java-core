994ae8c7f36d7ca98453965c6178b3532a563ff3
Vivek Ajbe <vajbe.tce@gmail.com>
2024-06-07 08:58:11 +0530

    major operations

--- ExampleRunnable.java
+++ ExampleRunnable.java
@@ -1,6 +1,7 @@
 class RunnableDemo implements Runnable {
-    private Thread t;
+    public Thread t;
     private String threadName;
+    boolean suspended = false;
 
     RunnableDemo(String name) {
         threadName = name;
@@ -13,7 +14,13 @@ class RunnableDemo implements Runnable {
         try {
             for (int i = 0; i < 4; i++) {
                 System.out.println("Thread: " + threadName + " i: " + i);
-                Thread.sleep(50);
+                Thread.sleep(300);
+
+                synchronized (this) {
+                    while (suspended) {
+                        wait();
+                    }
+                }
             }
         } catch (InterruptedException e) {
             System.out.println("Thread " + threadName + " interupted");
@@ -28,6 +35,15 @@ class RunnableDemo implements Runnable {
             t.start();
         }
     }
+
+    public void suspend() {
+        suspended = true;
+    }
+
+    synchronized void resume() {
+        suspended = false;
+        notify();
+    }
 }
 
 public class ExampleRunnable {
@@ -37,6 +53,32 @@ public class ExampleRunnable {
 
         RunnableDemo d2 = new RunnableDemo("Thread - 2");
         d2.start();
+
+        try {
+            Thread.sleep(1000);
+            d1.suspend();
+            System.out.println("Suspending thread 1");
+            Thread.sleep(1000);
+            d1.resume();
+            System.out.println("Resuming first thread");
+
+            d2.suspend();
+            System.out.println("Suspending thread 2");
+            Thread.sleep(1000);
+            d2.resume();
+            System.out.println("Resuming second thread");
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted");
+        }
+        
+        try {
+            System.out.println("Waiting for threads to finish");
+            d1.t.join();
+            d2.t.join();
+        }catch(InterruptedException e) {
+            System.out.println(e);
+        }
+        System.out.println("Main thread done");
     }
 
 }
