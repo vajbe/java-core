e1855ef91f2b6dc78718721eaacaf9cea77ba5c9
Vivek Ajbe <vajbe.tce@gmail.com>
2025-03-22 07:25:40 +0530

    fixed thread

---
+++ Concurrency/ThreadPools/FixedThread.java
@@ -0,0 +1,37 @@
+package Concurrency.ThreadPools;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class Job implements Runnable {
+    int id;
+
+    Job(int i) {
+        this.id = i;
+    }
+
+    @Override
+    public void run() {
+        long id = Thread.currentThread().getId();
+        long duration = (long) Math.random() + 3;
+        try {
+            Thread.sleep(4);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println("Thread: " + id + " Duration: " + duration);
+
+    }
+
+}
+
+public class FixedThread {
+    public static void main(String[] args) {
+        ExecutorService es = Executors.newFixedThreadPool(2);
+        for (int i = 0; i < 12; i++) {
+            es.submit(new Job(i));
+        }
+        /* es.shutdown(); */
+    }
+}
