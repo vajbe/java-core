f19f86f10575b8ab7e355bf9c639c94d6fcc0e88
Vivek Ajbe <vajbe.tce@gmail.com>
2025-03-27 08:42:50 +0530

    recent counter

---
+++ Problems/NumRecentCalls/RecentCounter.java
@@ -0,0 +1,33 @@
+package Problems.NumRecentCalls;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class RecentCounter {
+    Queue<Integer> q;
+
+    RecentCounter() {
+        this.q = new LinkedList<>();
+    }
+
+    int ping(int t) {
+        this.q.add(t);
+        int start = t - 3000;
+        int end = t;
+        int counter = 0;
+        for (int a : this.q) {
+            if (start <= a && a <= end) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        RecentCounter r = new RecentCounter();
+        System.out.println(r.ping(1));
+        System.out.println(r.ping(100));
+        System.out.println(r.ping(3001));
+        System.out.println(r.ping(3002));
+    }
+}
---
+++ Problems/TwinSumLL/TwinSum.java
@@ -0,0 +1,56 @@
+package Problems.TwinSumLL;
+
+import java.util.Stack;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class TwinSum {
+    public int pairSum(ListNode head) {
+
+        int midIndex = 0;
+        ListNode midNode = head;
+        ListNode fastNode = head;
+
+        if (head.next.next == null) {
+            return head.val + head.next.val;
+        }
+
+        while (fastNode.next.next != null) {
+            midNode = midNode.next;
+            fastNode = fastNode.next.next;
+            midIndex++;
+        }
+        midNode = midNode.next;
+        Stack<ListNode> s = new Stack<>();
+        while (midNode != null) {
+            s.push(midNode);
+            midNode = midNode.next;
+        }
+        int i = 0;
+        int maxSum = 0;
+        ListNode curr = head;
+        while (i < midIndex + 1) {
+            ListNode currEl = s.pop();
+            maxSum = Math.max(maxSum, currEl.val + curr.val);
+            curr = curr.next;
+            i++;
+
+        }
+        return maxSum;
+    }
+}
