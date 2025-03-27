242baae3446247a10ace2034a7b7ee3d50b2b390
Vivek Ajbe <vajbe.tce@gmail.com>
2025-03-27 17:46:10 +0530

    indegree outdegree basic example

---
+++ Problems/TownJudge/TownJudge.java
@@ -0,0 +1,34 @@
+package Problems.TownJudge;
+
+public class TownJudge {
+
+    int findJudge(int n, int trusts[][]) {
+        if (n == 1 && trusts.length == 0)
+            return 1;
+
+        int[] outDegree = new int[n + 1];
+        int[] inDegree = new int[n + 1];
+
+        for (int[] relation : trusts) {
+            int a = relation[0];
+            int b = relation[1];
+
+            outDegree[a]++;
+            inDegree[b]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (inDegree[i] == n - 1 && outDegree[i] == 0) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        TownJudge judge = new TownJudge();
+        int[][] arr = { { 1, 3 }, { 2, 3 }, { 3, 1 } };
+        System.out.println(judge.findJudge(3, arr));
+    }
+}
