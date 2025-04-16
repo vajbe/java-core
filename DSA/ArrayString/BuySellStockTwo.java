1283199fbc4848efbf47f769da9ca86ceb160157
Vivek Ajbe <vajbe.tce@gmail.com>
2025-04-16 07:09:19 +0530

    buy sell

--- DSA/ArrayString/BuySellStockTwo.java
+++ DSA/ArrayString/BuySellStockTwo.java
@@ -2,5 +2,24 @@
 package DSA.ArrayString;
 
 public class BuySellStockTwo {
-    
+    public int maxProfit(int[] prices) {
+        int buyPrice = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (buyPrice < prices[i]) {
+                profit += (prices[i] - buyPrice);
+            }
+            buyPrice = prices[i];
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        BuySellStockTwo s = new BuySellStockTwo();
+        System.out.println(s.maxProfit(new int[] { 7, 1, 5, 3, 6, 4 }));
+        System.out.println(s.maxProfit(new int[] { 1, 2, 3, 4, 5 }));
+        System.out.println(s.maxProfit(new int[] { 7, 6, 4, 3, 1 }));
+
+    }
 }
